ef8484534e59e0b4849bb120aebc6926a2ef7fcd
“Christiangit config --global user.email “cristianoliveros_27@hotmail.com <cristianoliveros_27@hotmail.com>
2015-05-23 23:29:58 -0500

    organización del codigo con angular

--- app/app.js
+++ app/app.js
@@ -2,70 +2,68 @@
 (function (){
 	var app = angular.module('restaurant',[] );
 
-	app.controller('menuController', function(){
-		this.menu ={
-			"items": [
-			            {
-			                "name": "coke",
-			                "qty": 20,
-			                "category": 0,
-			                "sizes": [
-			                "small",
-			                "large"
-			                ]
-			            },
-			            {
-			                "name": "pepsi",
-			                "qty": 20,
-			                "category": 0,
-			                "sizes": [
-			                "small",
-			                "large"
-			                ]
-			            },
-			            {
-			                "name": "water",
-			                "qty": 20,
-			                "category": 0,
-			                "sizes": [
-			                "small",
-			                "large"
-			                ]
-			            },
-			            {
-			                "name": "hamburger",
-			                "qty": 40,
-			                "category": 1,
-			                "sizes": [
-			                "small",
-			                "large"
-			                ]
-			            },
-			            {
-			                "name": "fries",
-			                "qty": 20,
-			                "category": 1,
-			                "sizes": [
-			                "small",
-			                "large"
-			                ]
-			            },
-			            {
-			                "name": "pizza",
-			                "qty": 20,
-			                "category": 1,
-			                "sizes": [
-			                "small",
-			                "large"
-			                ]
-			            }
-			        ],
-			        "categories": [
-			            "drinks",
-			            "junk food"
-			        ]
+	pp.controller('mnueController', function(){
+		this.menu = {
+			 "categoria": [
+        	{
+            "id": "001",
+            "nombre": "bebida",
+            "producto": [
+                {
+                    "nombre": "Coca cola",
+                    "Descripcion": "gaseosa helada",
+                    "precio": "1200"
+                },
+                {
+                    "nombre": "Limonada de coco",
+                    "Descripcion": "limonada con zumo de coco",
+                    "precio": "3200"
+                },
+                {
+                    "nombre": "CLub Colombia",
+                    "Descripcion": "cerveza club colombia",
+                    "precio": "2000"
+                }
+            ]
+        },
+        {
+            "id": "002",
+            "nombre": "Almuerzo",
+            "producto": [
+                {
+                    "nombre": "Menu del día",
+                    "Descripcion": "pescado, arroz con coco y patacon",
+                    "precio": 12000
+                },
+                {
+                    "nombre": "bandeja paisa",
+                    "Descripcion": "arroz, frijo, chicharron, patacon",
+                    "precio": 15200
+                },
+                {
+                    "nombre": "ajiaco",
+                    "Descripcion": "SOpa bogotana",
+                    "precio": 8000
+                }
+            ]
+        }
+    ]
 		};
 	});
+	app.directive('menuTop', function() {
+	    return {
+	     restrict : 'E',
+	     templateUrl : 'partials/header-menu.html'
+	    }
+	});
+		app.directive('piePagina', function() {
+	    return {
+	     restrict : 'E',
+	     templateUrl : 'partials/footer.html'
+	    }
+	});
+
+
 
 
 })();
@@ -73,70 +71,5 @@
 
 
 
-/*
-{
-    "menu": {
-        "items": [
-            {
-                "name": "coke",
-                "qty": 20,
-                "category": 0,
-                "sizes": [
-                    "small",
-                    "large"
-                ]
-            },
-            {
-                "name": "pepsi",
-                "qty": 20,
-                "category": 0,
-                "sizes": [
-                    "small",
-                    "large"
-                ]
-            },
-            {
-                "name": "water",
-                "qty": 20,
-                "category": 0,
-                "sizes": [
-                    "small",
-                    "large"
-                ]
-            },
-            {
-                "name": "hamburger",
-                "qty": 40,
-                "category": 1,
-                "sizes": [
-                    "small",
-                    "large"
-                ]
-            },
-            {
-                "name": "fries",
-                "qty": 20,
-                "category": 1,
-                "sizes": [
-                    "small",
-                    "large"
-                ]
-            },
-            {
-                "name": "pizza",
-                "qty": 20,
-                "category": 1,
-                "sizes": [
-                    "small",
-                    "large"
-                ]
-            }
-        ],
-        "categories": [
-            "drinks",
-            "junk food"
-        ]
-    }
-}
 
-*/
+
