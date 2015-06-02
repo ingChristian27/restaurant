1882ef3f4b54b439412803f39bb863a9f8c7b06d
Christian <cristianoliveros_27@hotmail.com>
2015-06-02 16:15:33 -0500

    creación de rutas

--- app/app.js
+++ app/app.js
@@ -1,9 +1,30 @@
 
 (function (){
 	var app = angular.module('restaurant',[
+			'ngRoute',
 			'restaurant.controller',
 			'restaurant.directivas'
 		] );
+
+
+
+	app.config(['$routeProvider', function($routeProvider){
+		$routeProvider
+
+			.when('/',{
+				templateUrl:'app/vistas/menu.html'
+			})
+			.when('/test',{
+				templateUrl:'app/vistas/test.html'
+			})
+			.otherwise({
+				redirectTo: '/'
+			})
+	}]);
+
+
+
+
 })();
 
 
